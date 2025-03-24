a1a2374891ba1e754c1325c4946eaed2d27781cc
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-03-24 20:56:30 +0000

    FEATURE: Set default mode to dark on layout component mount

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import '../app.css';
-	import { ModeWatcher } from 'mode-watcher';
+	import { ModeWatcher, setMode } from 'mode-watcher';
 
 	import { Footer } from '$lib/components/ui/Footer/index.js';
 	import { Navbar } from '$lib/components/ui/Navbar/index.js';
+	import { onMount } from 'svelte';
 	let active: string | null = null;
 	let { children } = $props();
+	onMount(() => {
+		setMode('dark');
+	});
 	// Navbar related
 </script>
 
