50b42dda5c3b8e7cc9b33e1071d0e42fc7ee4a5a
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-03-31 03:48:13 +0000

    FEATURE: Update SponsorsCards component for improved layout and responsiveness

--- src/lib/components/ui/MagicCard/SponsorsCards.svelte
+++ src/lib/components/ui/MagicCard/SponsorsCards.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <section class="mx-[10%]">
-	<h1 class="my-8 text-7xl font-bold">Nos Partenaires :</h1>
-	<section class=" mb-10 grid grid-cols-1 gap-10 lg:grid-cols-2">
-		<div class="h-44 lg:h-64">
+	<h1 class="mb-16 text-5xl font-bold">Nos Partenaires :</h1>
+	<section class=" mb-10 grid grid-rows-1 gap-10 lg:grid-cols-2">
+		<div class="h-32 lg:h-48">
 			<MagicCard
 				class="group cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl transition-all duration-300 hover:border-[#AF3485]"
 				gradientColor="#7f486dB5"
@@ -14,11 +14,11 @@
 				<div
 					class="flex items-center justify-center gap-1.5 font-semibold transition-all duration-500 group-hover:text-[#AF3485]"
 				>
-					<img class="p-10 lg:p-32" src="{base}/code_212.png" alt="" srcset="" />
+					<img class="w-48 lg:w-80" src="{base}/code_212.png" alt="" srcset="" />
 				</div>
 			</MagicCard>
 		</div>
-		<div class="h-44 lg:h-64">
+		<div class="h-32 lg:h-48">
 			<MagicCard
 				class="group cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl transition-all duration-300 hover:border-[#0D4192]"
 				gradientColor="#0D4192BF"
@@ -26,11 +26,11 @@
 				<div
 					class="flex items-center justify-center gap-1.5 font-semibold transition-all duration-500 group-hover:text-[#0D4192]"
 				>
-					<img class="p-16 lg:p-44" src="{base}/ounousc.png" alt="" srcset="" />
+					<img class="w-36 lg:w-44" src="{base}/ounousc.png" alt="" srcset="" />
 				</div>
 			</MagicCard>
 		</div>
-		<div class="h-44 lg:h-64">
+		<div class="h-32 lg:h-48">
 			<MagicCard
 				class="group cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl transition-all duration-300 hover:border-white"
 				gradientColor="#ffffff54"
@@ -38,11 +38,11 @@
 				<div
 					class="flex items-center justify-center gap-1.5 font-semibold transition-all duration-500 group-hover:text-white"
 				>
-					<img class="w-44 p-2" src="{base}/oja_logo.png" alt="" srcset="" />
+					<img class="w-28 lg:w-44" src="{base}/oja_logo.png" alt="" srcset="" />
 				</div>
 			</MagicCard>
 		</div>
-		<div class="h-44 lg:h-64">
+		<div class="h-32 lg:h-48">
 			<MagicCard
 				class="group cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl transition-all duration-300 hover:border-[#098B36]"
 				gradientColor="#098B3654"
@@ -50,7 +50,20 @@
 				<div
 					class="flex items-center justify-center gap-1.5 font-semibold transition-all duration-500 group-hover:text-[#0D4192]"
 				>
-					<img class="p-10 lg:p-44" src="{base}/emsi_logo.png" alt="" srcset="" />
+					<img class="w-48 lg:w-80" src="{base}/emsi_logo.png" alt="" srcset="" />
+				</div>
+			</MagicCard>
+		</div>
+
+		<div class="h-32 lg:h-48">
+			<MagicCard
+				class="group cursor-pointer flex-col items-center justify-center whitespace-nowrap text-4xl shadow-2xl transition-all duration-300 hover:border-[#F4C90054]"
+				gradientColor="#F4C90054"
+			>
+				<div
+					class="flex items-center justify-center gap-1.5 font-semibold transition-all duration-500 group-hover:text-[#0D4192]"
+				>
+					<img class="w-28 lg:w-44" src="{base}/fstt_logo.png" alt="" srcset="" />
 				</div>
 			</MagicCard>
 		</div>
